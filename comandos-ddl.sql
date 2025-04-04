e987ce6d8e92365a65ffc685bd95df2973fc2cb9
GuustavoPetry <ogustavopetry@gmail.com>
2025-04-04 10:00:36 -0300

    comandos ddl

---
+++ comandos-ddl.sql
@@ -0,0 +1,8 @@
+-- Altera uma tabela já existente, adicionando uma nova coluna:
+ALTER TABLE nome_tabela ADD nome_nova_coluna INT;
+
+-- Deleta uma tabela:
+DROP TABLE nome_tabela;
+
+-- Exclui todos os registros de uma tabela (zera o contador de auto_increment da tabela):
+TRUNCATE TABLE nome_tabela;
